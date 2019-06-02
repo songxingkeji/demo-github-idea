08dc998f6ce81615044a9b54309bba8f74dc225c
zhangxuejiao <zhang.xuejiao@sunsheen.cn>
2019-06-02 19:01:29 +0800

    Initial commit

---
+++ src/main/java/com/example/demogithubidea/DemoGithubIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogithubidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubIdeaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demogithubidea/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.demogithubidea.demo;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("test github....");
+    }
+}
